07de2ba48931027012973be96d219158a4e9e52e
Trevis McGill <trevismcgill25@gmail.com>
2020-08-26 20:14:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ def save
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
 
-def self.create(name)
+def self.create(hash)
   student = Student.new(name, grade)
   student.save
   student
