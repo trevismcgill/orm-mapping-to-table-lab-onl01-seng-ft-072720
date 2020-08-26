ec1359f76627273b9de02388027c24b15a422bd8
Trevis McGill <trevismcgill25@gmail.com>
2020-08-26 19:50:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,6 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
